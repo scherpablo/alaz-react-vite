2e0536b4eaea757cc4ab4c66b6a4f3b087a4e8d7
scherpablo <scherpablo@gmail.com>
2023-09-22 17:45:52 -0300

    analitycs vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter  } from 'react-router-dom'
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
 )

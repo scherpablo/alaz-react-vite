8c9b077ea255c74602a23c9165fbba66760c952c
scherpablo <scherpablo@gmail.com>
2023-09-21 23:20:08 -0300

    update services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,32 +6,32 @@ const Services = () => {
     {
       icon: <img src="img/services/camion.png" className={styles.imgIcon}></img>,
       title: "Distribución",
-      info: "Si necesita de un proveedor estable, eficiente y eficaz que responda en tiempo y forma para realizar la distribución y el traslado de su mercadería, correo interno y/o insumos, ya sea entre sus clientes, proveedores o bien entre las distintas plantas si de una empresa se trata. Para ello diseñamos un servicio de distribución que cumpla con sus expectativas a través de la información que Ud. nos brinde y nuestra experiencia en el campo logístico.",
+      info: "Si necesita de un proveedor estable, eficiente y eficaz que responda en tiempo y forma para realizar la distribución y el traslado de su mercadería, correo interno y/o insumos. Para ello diseñamos un servicio de distribución que cumpla con sus expectativas a través de la información que Ud. nos brinde y nuestra experiencia en logística."
     },
     {
       icon: <img src="img/services/repartidor.png" className={styles.imgIcon}></img>,
       title: "Puerta a Puerta",
-      info: "Para que su negocio crezca tiene que tener la seguridad y confianza de que sus pedidos llegan en tiempo y forma a los destinos que Ud. desea. Para esto le ofrecemos este servicio que consiste en retirar y entregar su mercadería en los puntos que solicite, ya sea su empresa o en un proveedor o cliente, cumpliendo con la puntualidad y seriedad que se requiere",
+      info: "Para que su negocio crezca tiene que tener la seguridad y confianza de que sus pedidos llegan en tiempo y forma a los destinos que Ud. desea. Para esto le ofrecemos este servicio que consiste en retirar y entregar su mercadería en los puntos que solicite, ya sea su empresa o en un proveedor o cliente, cumpliendo con la puntualidad y seriedad que se requiere"
     },
     {
       icon: <img src="img/services/redespacho.png" className={styles.imgIcon}></img>,
       title: "Redespacho",
-      info: "Este Servicio se implementa para poder llevar su mercadería a los transportes o expresos con los que su empresa se maneja habitualmente para envíos al interior del país.  Se realiza en el día, con retiro y entrega a domicilio garantizando la seguridad de su envío y su arribo a destino.",
+      info: "Este Servicio se implementa para poder llevar su mercadería a los transportes o expresos con los que su empresa se maneja habitualmente para envíos al interior del país.  Se realiza en el día, con retiro y entrega a domicilio garantizando la seguridad de su envío y su arribo a destino con la responsabilidad y el compromiso que nos caracterizan."
     },
     {
       icon: <img src="img/services/reembolso.png" className={styles.imgIcon}></img>,
       title: "Contrareembolso",
-      info: "Le damos la posibilidad al cliente de utilizar este servicio conjuntamente con el envío de su producto, de esa manera puede enviar la mercadería y en el mismo momento de la entrega se le cobra el total de su Factura haciéndonos cargo del cobro y pago del contra reembolso.",
+      info: "Le damos la posibilidad al cliente de utilizar este servicio conjuntamente con el envío de su producto, de esa manera puede enviar la mercadería y en el mismo momento de la entrega se le cobra el total de su Factura haciéndonos cargo del cobro y pago del contra reembolso."
     },
     {
       icon: <img src="img/services/seguro.png" className={styles.imgIcon}></img>,
       title: "Seguro mercadería",
-      info: "Los envíos que contengan “mercaderías” con un valor determinado podrán ser asegurados durante todo el trayecto: desde el retiro hasta la entrega al destinatario.",
+      info: "Los envíos que contengan “mercaderías” con un valor determinado podrán ser asegurados durante todo el trayecto: desde el retiro hasta la entrega al destinatario. De esta manera cuidamos tu mercadería y nos aseguramos de estar protegidos ante cualquier eventualidad."
     },
     {
       icon: <img src="img/services/inversa.png" className={styles.imgIcon}></img>,
       title: "Logística Inversa",
-      info: "Gestionamos el retorno de productos y devoluciones de manera eficiente, reduciendo costos y mejorando la satisfacción del cliente.",
+      info: "Gestionamos el retorno de productos y devoluciones de manera eficiente, reduciendo costos y mejorando la satisfacción del cliente. Realizamos estrictos controles de calidad para garantizar que la mercadería esté en condiciones óptimas antes de su distribucíon."
     },
   ];
 

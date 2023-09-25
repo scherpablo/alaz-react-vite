bf3f9a8148ed36824dbbb2d0e9f480a114195dfb
scherpablo <scherpablo@gmail.com>
2023-09-25 10:47:11 -0300

    button close update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -19,45 +19,33 @@ const Gallery = () => {
         <h2 className={`${styles.title} ${styles.galleryTitle}`}>Galeria de Imagenes</h2>
         <article className={styles.galleryContent}>
           <img
-            // src="./public/img/gallery/gallery01.png"
             src="/img/gallery/gallery01.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery01.png")}
             onClick={() => openModal("/img/gallery/gallery01.png")}
           />
           <img
-            // src="./public/img/gallery/gallery02.png"
             src="/img/gallery/gallery02.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery02.png")}
             onClick={() => openModal("/img/gallery/gallery02.png")}
           />
           <img
-            // src="./public/img/gallery/gallery03.png"
             src="/img/gallery/gallery03.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery03.png")}
             onClick={() => openModal("/img/gallery/gallery03.png")}
           />
           <img
-            // src="./public/img/gallery/gallery04.png"
             src="/img/gallery/gallery04.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery04.png")}
             onClick={() => openModal("/img/gallery/gallery04.png")}
           />
           <img
-            // src="./public/img/gallery/gallery05.png"
             src="/img/gallery/gallery05.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery05.png")}
             onClick={() => openModal("/img/gallery/gallery05.png")}
           />
           <img
-            // src="./public/img/gallery/gallery06.png"
             src="/img/gallery/gallery06.png"
             alt=""
-            // onClick={() => openModal("./public/img/gallery/gallery06.png")}
             onClick={() => openModal("/img/gallery/gallery06.png")}
           />
         </article>

f9f7b52cdccc96d27235f74a6d8dab52e534f95f
scherpablo <scherpablo@gmail.com>
2023-09-25 11:08:00 -0300

    update buttons sections

--- src/components/DirectDivision/DirectDivision.jsx
+++ src/components/DirectDivision/DirectDivision.jsx
@@ -1,4 +1,4 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./DirectDivision.module.css";
 
 const DirectDivision = () => {
@@ -19,21 +19,23 @@ const DirectDivision = () => {
             Contamos con más de 10 años de experiencia en el mercado de la venta
             directa, no solo en la parte logística sino en la atención al
             cliente, lo cual nos permite brindar un servicio específico para
-            cubrir las necesidades de sus clientes. 
+            cubrir las necesidades de sus clientes.
             <br />
-            Sabemos lo importante que es
-            para las directoras de ventas la cadena de distribución con respecto
-            a los tiempos de entrega, a la atención cordial y personalizada y al
-            cuidado de sus productos, para poder lograr sus objetivos
-            personales. 
+            Sabemos lo importante que es para las directoras de ventas la cadena
+            de distribución con respecto a los tiempos de entrega, a la atención
+            cordial y personalizada y al cuidado de sus productos, para poder
+            lograr sus objetivos personales.
             <br />
-            Es por esto que contamos con la estructura, los móviles
-            y el personal capacitado para ser un socio estratégico para su
-            empresa.
+            Es por esto que contamos con la estructura, los móviles y el
+            personal capacitado para ser un socio estratégico para su empresa.
           </p>
-          <Link to="https://whatsapp.com/" target="_blank" rel="noreferer"><a className={`${styles.cta} ${styles.ctaInfo}`}>
-            Contacto
-          </a></Link>
+          <Link to="https://whatsapp.com/" target="_blank" rel="noreferer">
+            <button
+              className={`${styles.cta} ${styles.ctaInfo} ${styles.ctaDivision}`}
+            >
+              Contacto
+            </button>
+          </Link>
         </div>
       </section>
     </>
--- src/components/Us/Us.jsx
+++ src/components/Us/Us.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import styles from "./Us.module.css";
 
 const Us = () => {
@@ -10,19 +10,23 @@ const Us = () => {
           <p className={styles.paragraph}>
             ALAZ LOGISTICA es un equipo apasionado por la logística y la
             excelencia en el servicio. Nos encontramos en pleno crecimiento y
-            con objetivos claros los cuales nos permiten mejorar día a día. 
+            con objetivos claros los cuales nos permiten mejorar día a día.
             <br />
-            En un mercado exigente y donde los tiempos de entrega son cada vez más
-            reducidos, ofrecemos a nuestros clientes un servicio de logística
-            integral. 
+            En un mercado exigente y donde los tiempos de entrega son cada vez
+            más reducidos, ofrecemos a nuestros clientes un servicio de
+            logística integral.
             <br />
-            Para ello contamos con unidades acordes a cada necesidad,
-            atención personalizada y la calidad y confianza que usted necesita
-            para poder cumplir con sus objetivos logísticos.
+            Para ello contamos con unidades acordes a cada necesidad, atención
+            personalizada y la calidad y confianza que usted necesita para poder
+            cumplir con sus objetivos logísticos.
           </p>
-          <Link to="https://whatsapp.com/" target="_blank" rel="noreferer"><a className={`${styles.cta} ${styles.ctaInfo}`}>
-            Contacto
-          </a></Link>
+          <Link to="https://whatsapp.com/" target="_blank" rel="noreferer">
+            <button
+              className={`${styles.cta} ${styles.ctaInfo} ${styles.ctaUs}`}
+            >
+              Contacto
+            </button>
+          </Link>
         </div>
         <img src="/img/us/logistica.png" alt="" className={styles.imgInfo} />
       </section>
